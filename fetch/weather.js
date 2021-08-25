841373278f8272cdcc7d5f549385dc935d9d9e5d
Fabio Pinatti <fpinatti@ciandt.com>
2021-08-25 17:31:12 -0300

    ignoring check weather json for now

--- fetch/weather.js
+++ fetch/weather.js
@@ -8,36 +8,36 @@ const timeToUpdate = 30 //in minutes
 
 const fetchWeather = async () => {
 	console.log('fetching weather...')
-	let jsonAge = await checkJsonAge()
-	console.log(jsonAge, timeToUpdate, 60 * timeToUpdate);
-	if (jsonAge > 60 * timeToUpdate || !jsonAge) {
-		try {
-			const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-			const options = {
-				method: 'GET'
-			}
-
-			const response = await fetch(endpoint, options)
-			const responseText = await response.json()
-			saveJson(responseText)
-
-		} catch (err) {
-			console.log(err)
-		}
-	} else {
-		console.log('Weather data is updated, no need to fetch again')
-	}
-}
-
-const checkJsonAge = async () => {
+	// let jsonAge = await checkJsonAge()
+	// console.log(jsonAge, timeToUpdate, 60 * timeToUpdate);
+	// if (jsonAge > 60 * timeToUpdate || !jsonAge) {
 	try {
-		const stats = fs.statSync(`./public/weather-${city}.json`)
-		const seconds = (new Date().getTime() - new Date(stats.mtime).getTime()) / 1000
-		return seconds
+		const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+		const options = {
+			method: 'GET'
+		}
+
+		const response = await fetch(endpoint, options)
+		const responseText = await response.json()
+		saveJson(responseText)
+
 	} catch (err) {
 		console.log(err)
 	}
+	// } else {
+	// 	console.log('Weather data is updated, no need to fetch again')
+	// }
 }
+
+// const checkJsonAge = async () => {
+// 	try {
+// 		const stats = fs.statSync(`./public/weather-${city}.json`)
+// 		const seconds = (new Date().getTime() - new Date(stats.mtime).getTime()) / 1000
+// 		return seconds
+// 	} catch (err) {
+// 		console.log(err)
+// 	}
+// }
 
 const saveJson = (data) => {
 	console.log('saving weather data...')
